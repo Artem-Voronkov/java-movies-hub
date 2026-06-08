ed101028d971bc9151a6cf2ba77bc626f13ba049
Artem <malibu2903@yandex.ru>
2026-06-08 08:09:46 +0300

    Исправления

--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -15,6 +15,7 @@ public class Movie {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -22,6 +23,7 @@ public class Movie {
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -29,6 +31,7 @@ public class Movie {
     public Integer getYear() {
         return year;
     }
+
     public void setYear(Integer year) {
         this.year = year;
     }
